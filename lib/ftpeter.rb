3d5b6a283bbda355717b058f9669b31c759b5345
Matthias Viehweger <kronn@kronn.de>
2015-10-11 20:22:34 +0200

    Add previous version of CLI-script

--- lib/ftpeter.rb
+++ lib/ftpeter.rb
@@ -1,5 +1,69 @@
 require "ftpeter/version"
+require "pathname"
 
+# TODO extract git-backend to detect changes
+# TODO extract lftp-backend to deploy files
+# TODO extract parameter-passing
+# TODO setup help and option-parsing
 module Ftpeter
-  # Your code goes here...
+  def go
+    # external arguments
+    host = ARGV[0] # the host to deploy to
+    dir  = ARGV[1] || '/' # the directory to change into
+    last = ARGV[2] || 'origin/master' # get the last deployed version and current version
+
+    # internal vars
+    lftp_script = []
+    lftp_fn = Pathname.new("./lftp_script").expand_path
+
+    # build up diff since last version
+    files = `git log #{last}... --name-status --oneline`.split("\n")
+    deleted = files.grep(/^[RD]/).map { |l| l.gsub(/^[RD]\s+/, '') }.uniq
+    changed = files.grep(/^[ACMR]/).map { |l| l.gsub(/^[ACMR]\s+/, '') }.uniq
+
+    # lftp connection header
+    lftp_script << "open #{host}"
+    lftp_script << "cd #{dir}"
+
+    # lftp file commands
+    lftp_script << deleted.map do |fn|
+      "rm #{fn}"
+    end
+    lftp_script << changed.map do |fn|
+      "put #{fn} -o #{fn}"
+    end
+
+    # write script to file
+    lftp_fn.open('w') do |f|
+      f << lftp_script.flatten.join("\n")
+      f << "\n"
+    end
+
+    puts "="*80
+    puts lftp_fn.read
+    puts "="*80
+    puts
+
+    if okay?
+      `lftp -f #{lftp_fn}`
+      lftp_fn.delete
+    else
+      puts "#{lftp_fn} is left for your editing pleasure"
+    end
+  end
+
+  def confirm(confirmation = 'yes')
+    $stderr.print '[yes, No] > '
+
+    if $stdin.gets.chomp != confirmation
+      abort("you did not enter '#{confirmation}', aborting")
+    else
+      true
+    end
+  end
+
+  def okay?
+    $stderr.puts "is this script okay?"
+    confirm('yes')
+  end
 end
