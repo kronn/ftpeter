7e3b52d47beaba3ed0b792257b5ce8e51ad6d8fc
Matthias Viehweger <kronn@kronn.de>
2016-09-02 07:43:09 +0200

    Explain the different groups of classes

--- lib/ftpeter.rb
+++ lib/ftpeter.rb
@@ -1,12 +1,16 @@
 # TODO check if needed here (they shouldn't be)
-require "ftpeter/version"
 require "pathname"
 
 # TODO setup help and option-parsing
 module Ftpeter
   autoload :CLI, "ftpeter/cli"
+  autoload :VERSION, "ftpeter/version"
+
+  # value classes
   autoload :Changes, "ftpeter/changes"
   autoload :Connection, "ftpeter/connection"
+
+  # collection of plugable components
   autoload :Transport, "ftpeter/transport"
   autoload :Backend, "ftpeter/backend"
 end
